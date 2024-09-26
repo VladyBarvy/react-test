f6986b9350020693213eafa3c8f74ef6dd33bf83
VladyBarvy <vladbarskoff@yandex.ru>
2024-09-26 21:26:51 +0300

    260924 keep going

--- react-test-project/src/App.jsx
+++ react-test-project/src/App.jsx
@@ -3,20 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// https://habr.com/ru/companies/ruvds/articles/428077/
+const Greetings = (props) => <div>Hey you! {props.firstName} {props.lastName}!</div>;
+
+function HiBorsy() {
+
+  return (
+    <p className="borsykun">
+      Let's do business!
+    </p> 
+  )
+}
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const titleDom =<h4 className="card-title">Hello!</h4>;
 
   return (
     
 
     <>
 
+
+
+
+<div>
+    <Greetings firstName="John" lastName="Smith" />
+  </div>
+
+
+
+    <div className="card">
+      <div className="card-body">
+        Hi!
+        {titleDom}
+      </div>
+    </div>
+
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> 
 
+      <div>
+        <button onClick={() => <HiBorsy />}>Biarsu</button>
+      </div>
+
+   
 
 {/*     
       <div>
